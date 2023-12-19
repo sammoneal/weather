d64edcb4c44b45fde0ae2d75987e3eefd831b931
Sam O'Neal <sam.oneal@gmail.com>
2023-12-19 01:23:17 -0800

    Support files.

---
+++ cli.py
@@ -0,0 +1,33 @@
+import sys
+from datetime import datetime
+from textwrap import fill
+
+from weather_api import WeatherAPI, geolocator
+
+location = geolocator.geocode(sys.argv[1], country_codes="US")
+
+weather = WeatherAPI(location.latitude, location.longitude)
+for item in weather.forecast:
+    if item["number"] == 1:
+        current = item
+
+print(
+    f'{"  "+weather.city+"  ":*^75}\n\n{current["name"]}: {fill(current["detailedForecast"],65)}\n'
+)
+
+for item, _ in zip(weather.hourly, range(24)):
+    time_stamp = item["startTime"]
+    time = datetime.strptime(time_stamp[:19], "%Y-%m-%dT%H:%M:%S")
+
+    short = item["shortForecast"]
+    temp = item["temperature"]
+
+    precip = item["probabilityOfPrecipitation"]["value"]
+    precip = precip if precip else 0
+    day = "Day" if item["isDaytime"] else "Night"
+
+    wind = item["windSpeed"]
+    wind_dir = item["windDirection"]
+
+    output = f'{time.month:<2}/{time.day:<2}  {time.hour%12:>2}:00{"AM" if time.hour < 12 else "PM"}  {day:<5}  {short:<30}  {temp:>3}F  {precip:>3}%  {wind:>6} {wind_dir:<3}'
+    print(output)
---
+++ conditions.py
@@ -0,0 +1,48 @@
+import time
+from weather_api import WeatherAPI, geolocator
+
+CITIES = [
+    "Boston",
+    "Orlando",
+    "Tucson",
+    "San Diego",
+    "Casper",
+    "Chyenne",
+    "Honolulu",
+    "Albany",
+    "Chicago",
+    "Fargo",
+    "Seattle",
+    "Houston",
+    "Topeka",
+    "Allentown",
+    "St. Louis",
+    "Boise",
+    "Portland",
+    "Mesa AZ",
+    "Las Vegas",
+    "Lexington",
+    "Denver",
+    "Tampa",
+    "Philadelphia",
+    "Detroit",
+]
+
+CONDITIONS = set()
+
+for city in CITIES:
+    try:
+        geo = geolocator.geocode(city, country_codes="US")
+        weather = WeatherAPI(geo.latitude, geo.longitude)
+        for item in weather.forecast:
+            condition = item["shortForecast"]
+            CONDITIONS.add(condition)
+        time.sleep(5)
+    except KeyError:
+        continue
+
+with open("test.txt", "w") as file:
+    sorted_cond = list(CONDITIONS)
+    sorted_cond.sort()
+    for condition in sorted_cond:
+        file.write(f"{condition}\n")
