6925719f8d955666894ae97a2d1b573371e70ab2
Sam O'Neal <sam.oneal@gmail.com>
2024-02-02 22:46:26 -0800

    Broke dashboard into multiple templates. Integrated some data from the weather object into templates. Modified the weather api class to find min and max values for use in chart legends.

--- weather_api/nws_api.py
+++ weather_api/nws_api.py
@@ -1,4 +1,4 @@
-import re
+import re, math
 from collections import namedtuple
 from itertools import chain
 from datetime import datetime
@@ -22,41 +22,57 @@ class NWS:
         self.forecast = self.get_forecast(self.metadata["properties"]["forecast"])
         self.hourly = self.get_forecast(self.metadata["properties"]["forecastHourly"])
 
-        self.max_temp = self.current['temperature']
+        self.max_temp = self.current["temperature"]
         self.min_temp = self.max_temp
+        self.max_wind = 40
 
-        #Process the response
+        # Process the response
         for item in chain(self.forecast, self.hourly):
-            #Time code
+            # Time code
             item["time"] = datetime.strptime(
                 item["startTime"][:19], "%Y-%m-%dT%H:%M:%S"
             )
-            #Icon map
+            # Icon map
             conditions = re.split("then", item["shortForecast"])
             try:
-                icon_set = conditions_map[conditions[0]]
+                icon_set = conditions_map[conditions[0].strip()]
                 if item["isDaytime"]:
                     item["icon"] = icon_set[0]
                 else:
                     item["icon"] = icon_set[-1]
             except LookupError:
                 item["icon"] = "wi-alien"
-            #Percipitation fix
+            # Percipitation fix
             precip = item["probabilityOfPrecipitation"]["value"]
-            precip = precip if precip else 0
-            #Wind icon
+            if not precip:
+                item["probabilityOfPrecipitation"]["value"] = 0
+            # Wind value
+            
+            # Wind icon
             item["windIcon"] = f"wi-towards-{item['windDirection'].lower()}"
-            if item['temperature'] > self.max_temp:
-                self.max_temp = item['temperature']
-            elif item['temperature'] < self.min_temp:
-                self.min_temp = item['temperature']
+            if item["temperature"] > self.max_temp:
+                self.max_temp = item["temperature"]
+            elif item["temperature"] < self.min_temp:
+                self.min_temp = item["temperature"]
 
+        # Round min and max for cleaner charts
+        step = 5
+        self.min_temp = math.floor(self.min_temp / step) * step
+        self.max_temp = math.ceil(self.max_temp / step) * step
+        self.max_wind = math.ceil(self.max_wind / step) * step
+        self.temp_spread = self.max_temp - self.min_temp
+        self.temp_labels = []
+        for i in range(1, 4):
+            self.temp_labels.append(round(i * 0.25 * self.temp_spread) + self.min_temp)
+        self.temp_labels.insert(0, self.min_temp)
+        self.temp_labels.append(self.max_temp)
+        self.temp_labels.reverse()
 
     @property
     def city(self):
         location = self.metadata["properties"]["relativeLocation"]["properties"]
         return f"{location['city']}, {location['state']}"
-    
+
     @property
     def current(self):
         return self.forecast[0]
@@ -65,3 +81,9 @@ class NWS:
     def get_forecast(url):
         weather = requests.get(url).json()
         return weather["properties"]["periods"]
+    
+    def round_statistics(self, step = 5):
+        self.min_temp = math.floor(self.min_temp / step) * step
+        self.max_temp = math.ceil(self.max_temp / step) * step
+        self.max_wind = math.ceil(self.max_wind / step) * step
+
